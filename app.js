f9c87a7911e730b0b51ad6ed10c0dff4eb3eef00
Zorko <zorkojaka@gmail.com>
2017-12-04 08:21:03 +0000

    popravek test

--- app.js
+++ app.js
@@ -324,11 +324,11 @@ function receivedMessage(event) {
   //var fs = require('fs');
 
   var options = {
-    hostname: '93.103.121.2',
-    port:8083,
-    Authorization: 'Basic YWRtaW46U2FsdXNkZDE=',
-    path: '/JS/Run/zway.devices[2].instances[1].commandClasses[37].Set(255)',
-    method: 'GET',
+    'hostname': '93.103.121.2',
+    'port': 8083,
+    'Authorization': 'Basic YWRtaW46U2FsdXNkZDE=',
+    'path': '/JS/Run/zway.devices[2].instances[1].commandClasses[37].Set(255)',
+    'method': 'GET',
   };
 
   var req = http.get(options, function(res){
