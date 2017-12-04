91f1f9c450a870dbd6093a012908730e29d9efde
Zorko <zorkojaka@gmail.com>
2017-12-04 08:26:23 +0000

    zna delat

--- app.js
+++ app.js
@@ -324,11 +324,13 @@ function receivedMessage(event) {
   //var fs = require('fs');
 
   var options = {
-    'hostname': '93.103.121.2',
-    'port': 8083,
-    'Authorization': 'Basic YWRtaW46U2FsdXNkZDE=',
-    'path': '/JS/Run/zway.devices[2].instances[1].commandClasses[37].Set(255)',
-    'method': 'GET',
+    hostname: '93.103.121.2',
+    port: 8083,
+    path: '/JS/Run/zway.devices[2].instances[1].commandClasses[37].Set(255)',
+    method: 'GET',
+    headers: {
+     'Authorization': 'Basic YWRtaW46U2FsdXNkZDE='
+    } 
   };
 
   var req = http.get(options, function(res){
