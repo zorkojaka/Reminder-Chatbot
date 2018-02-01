37b80018a1e47b6081a64295b8f2b4509d380841
Zorko <zorkojaka@gmail.com>
2018-02-01 14:34:56 +0000

    dodal sliko

--- app.js
+++ app.js
@@ -189,7 +189,7 @@ function HTMLgen(){
   for(a=0;a<ElementID.length;a++){
     page+="<tr><th>"+ElementID[a]+"</th><th>1</th><th>"+ElementName[a]+"</th><th>"+RoomName[a]+"</th><th>"+nastavljenavrednost[a]+"</th><th>"+enota[a]+"</th></tr>";
   }
-  page+="</table></html>"
+  page+="</table><div><img href='/public/res/Nacrt-hise.png'/> </div></html>"
   return page;
 }
 
