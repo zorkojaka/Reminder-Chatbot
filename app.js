74a7777fa7c496c7989a4b37119fe3fa6ea2d163
Zorko <zorkojaka@gmail.com>
2018-02-01 14:38:26 +0000

    popravil sliko

--- app.js
+++ app.js
@@ -189,7 +189,7 @@ function HTMLgen(){
   for(a=0;a<ElementID.length;a++){
     page+="<tr><th>"+ElementID[a]+"</th><th>1</th><th>"+ElementName[a]+"</th><th>"+RoomName[a]+"</th><th>"+nastavljenavrednost[a]+"</th><th>"+enota[a]+"</th></tr>";
   }
-  page+="</table><div><img href='/res/Nacrt-hise.png'/></div></html>"
+  page+="</table><div><img src='/res/Nacrt-hise.png' alt='Razporeditev elementov po sobah'/></div></html>"
   return page;
 }
 
