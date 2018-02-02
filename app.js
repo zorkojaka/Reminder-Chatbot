ed51669e43d2faef68dd81386ffc3a26fb03c688
Zorko <zorkojaka@gmail.com>
2018-02-02 13:34:00 +0000

    black

--- app.js
+++ app.js
@@ -206,7 +206,7 @@ function HTMLgentest(){
   var page="<html><head><title>Prikaz</title><meta http-equiv='refresh' content='2'/></head><h1>TABELA ELEMENTOV</h1><table><tr border='1'><th>ID</th><th>Instanca</th><th>Ime</th><th>Soba</th><th>Vrednost</th><th>Enota</th><th>Željena vrednost</th></tr>";
   for(a=0;a<ElementID.length;a++){
     if(zeljenavrednost[a]==vrednosti[a]){
-      page+="<tr><th>"+ElementID[a]+"</th><th>1</th><th>"+ElementName[a]+"</th><th>"+RoomName[a]+"</th><th>"+nastavljenavrednost[a]+"</th><th>"+enota[a]+"</th><th>"+zeljenavrednost[a]+"</th></tr>";
+      page+="<tr><th>"+ElementID[a]+"</th><th>1</th><th>"+ElementName[a]+"</th><th>"+RoomName[a]+"</th><th>"+nastavljenavrednost[a]+"</th><th>"+enota[a]+"</th><th style='color:black'>"+zeljenavrednost[a]+"</th></tr>";
     }else{
       page+="<tr><th>"+ElementID[a]+"</th><th>1</th><th>"+ElementName[a]+"</th><th>"+RoomName[a]+"</th><th>"+nastavljenavrednost[a]+"</th><th>"+enota[a]+"</th><th style='color:red'>"+zeljenavrednost[a]+"</th></tr>";
     }
